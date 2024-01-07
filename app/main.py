3ba91eaa2d1cd008879d5df543d4e95a7355ffc6
Guillermo Roman <gromande@hawk.iit.edu>
2024-01-07 14:51:23 -0600

    Updating Dockerfile

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
+import socket
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python!"
+    return "Hello from Python! " + socket.gethostname()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
